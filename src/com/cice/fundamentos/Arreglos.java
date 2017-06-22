a3bdb1ceb30cb9dd15d9f68f44db1dcabd4ac81b
Manu <Manu@PC-MANU>
2017-06-22 21:37:54 +0200

    (INITIAL COMMIT)

--- src/com/cice/fundamentos/Arreglos.java
+++ src/com/cice/fundamentos/Arreglos.java
@@ -16,6 +16,33 @@ public class Arreglos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    //Declaraciones de arrays
+        int[] numeros={1,2};
+        int otrosNumeros[];
+        
+        int[][] bidi={{1,2},{3,4}};
+        
+        int [][][] tridi={{{1,2},{3,4}},{{5,6},{7,8}},{{9,10},{11,12}}};
+            int prueba=tridi[2][1][0];
+            int[]edades[]=new int [2][2];
+            String[] tipos[][]=new String[2][2][2];
+            tipos[0][0][1]="25";
+            
+           
+             
+            
+             for (String[][] c:tipos){
+                for(String[] d:c){
+                    for (String v:d){
+                        System.out.println(v);
+                        
+                    }
+                }
+                
+            }
+            
+            
+       System.out.println(prueba);
     }
     
     
